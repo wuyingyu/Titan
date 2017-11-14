6418cd1f3e1996a4eb6d120cf6095e4e67fabcad
wuyingyu <yingyuwu@usc.edu>
2017-11-13 21:03:56 -0800

    connect to db

--- src/rpc/ItemHistory.java
+++ src/rpc/ItemHistory.java
@@ -3,6 +3,7 @@ package rpc;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,27 +15,45 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import db.MySQLConnection;
+import entity.Item;
+
 /**
  * Servlet implementation class ItemHistory
  */
 @WebServlet("/history")
 public class ItemHistory extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ItemHistory() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ItemHistory() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String userId = request.getParameter("user_id");
+		JSONArray array = new JSONArray();
+
+		MySQLConnection conn = new MySQLConnection();
+		Set<Item> items = conn.getFavoriteItems(userId);
+		for (Item item : items) {
+			JSONObject obj = item.toJSONObject();
+			try {
+				obj.append("favorite", true);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			array.put(obj);
+		}
+		RpcHelper.writeJsonArray(response, array);
+
 	}
 
 	/**
@@ -44,25 +63,50 @@ public class ItemHistory extends HttpServlet {
 		// TODO Auto-generated method stub
 		try {
 			JSONObject input = RpcHelper.readJsonObject(request);
-			if (input.has("user_id") && input.has("visited")) {
-				String userId = (String) input.get("user_id");
-				JSONArray array = (JSONArray) input.get("visited");
-				List<String> visitedEvents = new ArrayList<>();
-				for (int i = 0; i < array.length(); i++) {
-					String itemId = (String) array.get(i);
-					visitedEvents.add(itemId);
-				}
+			String userId = input.getString("user_id");
+			JSONArray array = (JSONArray) input.get("favorite");
 
-				RpcHelper.writeJsonObject(response,
-						new JSONObject().put("status", "OK"));
-			} else {
-				RpcHelper.writeJsonObject(response,
-						new JSONObject().put("status", "Invalid Parameters"));
+			List<String> histories = new ArrayList<>();
+			for (int i = 0; i < array.length(); i++) {
+				String itemId = (String) array.get(i);
+				histories.add(itemId);
 			}
+
+			MySQLConnection conn = new MySQLConnection();
+			conn.setFavoriteItems(userId, histories);
+
+			RpcHelper.writeJsonObject(response, new JSONObject().put("result", "SUCCESS"));
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 
 	}
 
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			JSONObject input = RpcHelper.readJsonObject(request);
+			String userId = input.getString("user_id");
+			JSONArray array = (JSONArray) input.get("favorite");
+
+			List<String> histories = new ArrayList<>();
+			for (int i = 0; i < array.length(); i++) {
+				String itemId = (String) array.get(i);
+				histories.add(itemId);
+			}
+
+			MySQLConnection conn = new MySQLConnection();
+			conn.unsetFavoriteItems(userId, histories);
+
+			RpcHelper.writeJsonObject(response, new JSONObject().put("result", "SUCCESS"));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
 }
+
+
